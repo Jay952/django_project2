eafb5ae2835d0f70ef770be46c7bf39a097deb66
[Jay952] <jaydeep4420@gmail.com>
2020-04-07 10:14:24 +0530

    third commit

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,4 +3,23 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello")
+    return HttpResponse("""<a href="https://web.whatsapp.com/"> Click Here to open Wattsapp web</a>""")
+
+def about(request):
+    return HttpResponse("<h1>Hii its me Jaydeep</h1>")
+
+def removepunc(request):
+    return HttpResponse("Removepunc")
+
+def capfirst(request):
+    return HttpResponse("Capitalize First")
+
+
+def newlineremove(request):
+    return HttpResponse("New line Remove")
+
+def spaceremove(request):
+    return HttpResponse("Space remove")
+
+def charcount(request):
+    return HttpResponse("Char Count")
